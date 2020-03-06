769f3489b71b4ebe7934cc4839c6fe061615b4d0
magdahime <magdalena.majkowska1@gmail.com>
2020-03-06 12:41:15 +0100

    Added recursive algorithm

--- algorithms.py
+++ algorithms.py
@@ -5,6 +5,7 @@ import operator
 
 
 def greedy_algorithm(donuts, stomach):
+    print("\nGREEDY ALGORITHM: \n")
     eaten_calories = 0
     donuts_weight = 0
     eaten_donuts = []
@@ -28,6 +29,28 @@ def greedy_algorithm(donuts, stomach):
     print(f"Total weight: {donuts_weight}")
 
 
+def donutbox(size, weight_tab, calories_tab, donuts_num):
+    if donuts_num == 0 or size == 0:
+        return 0
+    if weight_tab[donuts_num-1] > size:
+        return donutbox(size, weight_tab, calories_tab, donuts_num-1)
+    else:
+        return (
+            max(calories_tab[donuts_num-1] + donutbox(size - weight_tab[donuts_num-1], weight_tab, calories_tab, donuts_num-1),
+                donutbox(size, weight_tab, calories_tab, donuts_num-1)))
+
+
+def recursive_solution(donuts, stomach):
+    print("\nRECURSIVE METHOD: \n")
+    weight_tab = [donut.get_weight() for donut in donuts]
+    calories_tab = [donut.get_calories() for donut in donuts]
+    donuts_num = len(calories_tab)
+    size = stomach.get_stomach_capacity()
+    eaten_calories = donutbox(size, weight_tab, calories_tab, donuts_num)
+    
+    print(f"Eaten calories: {eaten_calories}")
+    
+
 def donut_generator(how_many):
     """Function to generate as many donuts as given in how_many"""
     donuts = []
@@ -40,3 +63,4 @@ def donut_generator(how_many):
 stomach = Stomach(400)
 donuts = donut_generator(20)
 greedy_algorithm(donuts, stomach)
+recursive_solution(donuts, stomach)
